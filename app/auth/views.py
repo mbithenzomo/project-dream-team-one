a27af7927646ed91e7d70e1ebe88a83cf7b61902
Germano Massullo <Germano0@users.noreply.github.com>
2020-03-30 17:59:14 +0200

    Edited views.py to fix import from forms.py
    Fixes bug
    ```
    $ flask run
    [...]
    
    Error: While importing "run", an ImportError was raised:
    
    Traceback (most recent call last):
      File "/usr/local/lib/python3.7/site-packages/flask/cli.py", line 240, in locate_app
        __import__(module_name)
      File "/home/a/python-flask-crud-1/run.py", line 6, in <module>
        app = create_app(config_name)
      File "/home/a/python-flask-crud-1/app/__init__.py", line 32, in create_app
        from .auth import auth as auth_blueprint
      File "/home/a/python-flask-crud-1/app/auth/__init__.py", line 5, in <module>
        from . import views
      File "/home/a/python-flask-crud-1/app/auth/views.py", line 5, in <module>
        from forms import LoginForm, RegistrationForm
    ModuleNotFoundError: No module named 'forms'
    ```

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,7 @@ from flask import flash, redirect, render_template, url_for
 from flask_login import login_required, login_user, logout_user
 
 from . import auth
-from forms import LoginForm, RegistrationForm
+from .forms import LoginForm, RegistrationForm
 from .. import db
 from ..models import Employee
 
